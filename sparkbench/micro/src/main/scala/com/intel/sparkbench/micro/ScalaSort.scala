4a44c16a137966ce045d3b05fec930237a3ce552
az79nefy <alexander.zap@stud.tu-darmstadt.de>
2017-10-13 13:00:42 +0200

    First commit

---
+++ sparkbench/micro/src/main/scala/com/intel/sparkbench/micro/ScalaSort.scala
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.hibench.sparkbench.micro
+
+import com.intel.hibench.sparkbench.common.IOCommon
+
+import org.apache.spark._
+import org.apache.spark.rdd.RDD
+
+import scala.reflect.ClassTag
+
+// ESpark imports
+import org.apache.spark.rdd.ProvRDD.rddToNewPairRDDFunctions
+import org.apache.spark.rdd.ProvRDD.rddToRDDProvFunctions
+import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
+
+// Often direct ProvRDD import also needed
+import org.apache.spark.rdd.ProvRDD
+
+object ScalaSort{
+  implicit def rddToHashedRDDFunctions[K : Ordering : ClassTag, V: ClassTag]
+         (rdd: ProvRDD[(K, V)]) = new ConfigurableOrderedRDDFunctions[K, V, (K, V)](rdd)
+
+  def main(args: Array[String]){
+    if (args.length != 2){
+      System.err.println(
+        s"Usage: $ScalaSort <INPUT_HDFS> <OUTPUT_HDFS>"
+      )
+      System.exit(1)
+    }
+    val sparkConf = new SparkConf().setAppName("ScalaSort")
+    val sc = new SparkContext(sparkConf)
+
+    val parallel = sc.getConf.getInt("spark.default.parallelism", sc.defaultParallelism)
+    val reducer  = IOCommon.getProperty("hibench.default.shuffle.parallelism")
+      .getOrElse((parallel / 2).toString).toInt
+
+    val io = new IOCommon(sc)
+    val data = new ProvRDD(io.load[String](args(0)).map((_, 1)))
+    val partitioner = new HashPartitioner(partitions = reducer)
+    val sorted = new ProvRDD(data.sortByKeyWithPartitioner(partitioner = partitioner)).map(_._1)
+
+    io.save(args(1), sorted)
+    sc.stop()
+  }
+}
